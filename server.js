6b86b435d967a736c0b21189f357ac217e8ce103
[juseche] <[juseche@motus.com]>
2023-08-16 12:15:04 -0500

    chore(cors): Access-Control-Allow-Origin all

--- server.js
+++ server.js
@@ -4,15 +4,11 @@ const port = 8000;
 
 import tool from "./tool.js";
 
-let CLIENT_URL = "https://adventure-inventory-api-production.up.railway.app";
-if (app.settings.env === `development`) {
-  CLIENT_URL = "http://localhost:3000";
-}
 // parse application/json
 app.use(express.json());
 
 app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", CLIENT_URL);
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers");
     next();
@@ -50,5 +46,4 @@ app.delete("/tools/:id", (req, res) => {
 
 app.listen(port, () => {
     console.log("adventure-inventory-api is running successfully")
-    console.log(`CLIENT_URL: ${CLIENT_URL}`);
 });
